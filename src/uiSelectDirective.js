0fa5b87fafa237a35ae04440cfbf5d0c748dea7f
U-YKF\aaxtmann <aaxtmann@pcNavAdamA.ykf.navtech.corp>
2015-03-18 15:01:45 -0400

    Fix for disabled fieldset bug

--- src/uiSelectDirective.js
+++ src/uiSelectDirective.js
@@ -70,7 +70,18 @@ uis.directive('uiSelect',
 
         attrs.$observe('disabled', function() {
           // No need to use $eval() (thanks to ng-disabled) since we already get a boolean instead of a string
-          $select.disabled = attrs.disabled !== undefined ? attrs.disabled : false;
+          $select.disabledFlag = attrs.disabled !== undefined ? attrs.disabled : false;
+          $select.disabled = $select.disabledFlag || $select.disabledSelector;
+        });
+
+        // When inside a disabled fieldset, disabledWatch element will be disabled
+        var disabledWatch = angular.element("<input type='hidden' aria-label=''></input>");
+        element.append(disabledWatch);
+
+        // Watch the disabled flag on disabledWatch, and set $select.disabled.
+        scope.$watch(function () { return disabledWatch.is(":disabled"); }, function (value) {
+          $select.disabledSelector = value;
+          $select.disabled = $select.disabledFlag || $select.disabledSelector;
         });
 
         attrs.$observe('resetSearchInput', function() {
